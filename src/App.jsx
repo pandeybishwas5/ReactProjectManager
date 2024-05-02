9f21b341c0bec0f70ace4f79508011e5e7699441
Bishwas Pandey <pandeybishwas5@gmail.com>
2024-05-02 22:53:51 +1000

    Add tassks functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjects from "./components/NoProjects";
 import ProjectsSideBar from "./components/ProjectsSideBar";
+import SelectedProject from "./components/SelectedProject";
 
 
 function App() {
@@ -9,8 +10,34 @@ function App() {
 
   const [ projectState, setProjectState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: [],
   });
+
+  function handleAddTask(text){
+    setProjectState( prevState => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+
+      return{
+        ...prevState,
+        tasks: [...prevState.tasks, newTask]
+      }
+    })
+  }
+
+  function handleDeleteTask(id){
+    setProjectState(prevState => {
+      return{
+        ...prevState,
+        tasks: prevState.tasks.filter(task => task.id !== id),
+      }; 
+    })
+  }
 
   function  handleAddProject() {
     setProjectState( prevState => {
@@ -47,17 +74,48 @@ function App() {
     })
   }
 
-  let content;
+  function handleSelectProject(id) {
+    setProjectState(prevState => {
+      return{
+        ...prevState,
+        selectedProjectId: id,
+      }; 
+    })
+  }
 
+  function handleDeleteProject() {
+    setProjectState(prevState => {
+      return{
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(project => project.id !== prevState.selectedProjectId),
+      }; 
+    })
+  }
+
+  const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId)
+  let content = 
+  <SelectedProject 
+    project={selectedProject} 
+    onDelete={handleDeleteProject} 
+    onAddTask={handleAddTask} 
+    onDeleteTask={handleDeleteTask}
+    tasks={projectState.tasks}
+  />;
+  
   if (projectState.selectedProjectId === null) {
     content = <NewProject onAdd={handleSaveProject} onCancel={handleCancelAddProject} />;
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjects onStartAddProject= {handleAddProject} />
-  }
+  } 
   
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar onStartAddProject= {handleAddProject} projects={projectState.projects}/>
+      <ProjectsSideBar 
+        onStartAddProject= {handleAddProject} 
+        projects={projectState.projects}
+        onSelectProject={handleSelectProject}
+      />
       {content}
     </main>
   );
---
+++ src/components/NewTask.jsx
@@ -0,0 +1,41 @@
+import { useState, useRef } from "react"
+import Modal from "./Modal";
+
+export default function NewTask({ onAdd }) {
+    const [enteredTask, setEnteredTask] = useState('');
+    const modal = useRef();
+    
+    function handleChange(event) {
+        setEnteredTask(event.target.value);
+    }
+
+    function handleClick() {
+        if (enteredTask.trim() === ''){
+            modal.current.open()
+        } else {
+            onAdd(enteredTask);
+            setEnteredTask('');
+        }
+       
+    }
+
+
+    return (
+        <>
+        <Modal ref={modal} buttonCaption='Close'>Please insert non empty task in input.</Modal>
+        <div className="flex items-center gap-4">
+            <input
+                type="text"
+                className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+                onChange= {handleChange}
+                value={enteredTask}
+            />
+            <button 
+                className="text-stone-700 hover:text-stone-950"
+                onClick={handleClick}
+            >Add Task</button>
+        </div>
+        </>
+        
+    )
+}
